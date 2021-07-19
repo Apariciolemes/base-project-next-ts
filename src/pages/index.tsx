a16f6c1287f6c02c4a36944c05c03bbb2d8dc67a
unknown <netolemes@hotmail.com>
2021-07-18 22:01:14 -0300

    feat(next): base project with tyepscript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import styleHome from '../styles/home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className={styleHome.title}>Home</h1>
+    <>
+      <Head>
+        <title>Ignews | Home</title>
+      </Head>
+      <h1 className={styleHome.title}>Home</h1>
+    </>
   )
 }
